eaad92a2e9dcb54a87d7ba631642c6997672a933
chaiyujin <chaiyujin@gmail.com>
2015-06-13 14:12:47 +0800

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,11 +9,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 //Route::get('welcome', 'WelcomeController@index');
-
 Route::get('home', 'HomeController@index');
-
 Route::get('/', 'PageController@index');
 Route::get('register', 'PageController@register');
 Route::get('login', 'PageController@getLogin');
@@ -22,12 +18,14 @@ Route::get('logout', 'PageController@getLogout');
 Route::post('login', 'PageController@postLogin');
 
 Route::get('check_list', ['middleware' => 'auth', 'uses' => 'PageController@check_list']);
+Route::get('check_list_double', ['middleware' => 'auth', 'uses' => 'PageController@check_list_double']);
+Route::get('check_list_history', ['middleware' => 'auth', 'uses' => 'PageController@check_list_history']);
+
 Route::get('fetch', ['middleware' => 'auth', 'uses' => 'PageController@fetch']);
 Route::get('main', ['middleware' => 'auth', 'uses' => 'PageController@main']);
 Route::get('search', ['middleware' => 'auth', 'uses' => 'PageController@search']);
 
 Route::resource('check', 'CheckController');
-
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
