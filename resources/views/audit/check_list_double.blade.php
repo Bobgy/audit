4b0080b77ae2cb472b69b546e9ca06e13922e8d7
Chenhenghong <Chenhenghong@users.noreply.github.com>
2015-06-13 22:45:17 +0900

    update error mode

--- resources/views/audit/check_list_double.blade.php
+++ resources/views/audit/check_list_double.blade.php
@@ -1,5 +1,6 @@
 @extends('audit.navbar')
 
+
 @section('content')
 
     <div style="height:80px"></div>
@@ -7,11 +8,13 @@
     <!-- Main container Begin-->
     <div class="container">
       <div class="row">
-        <div class="flowColumn2">
-          <div class="select">
+		<div class="flowColumn2">		
+		  <div class="select">	
             <a href="check_list" id="unprocessed_check_btn"> 未处理账单 </a>
             <div class="crossLine"></div>
             <a href="check_list_double" id="double_check_btn"> 二次审核 </a>
+			<div class="crossLine"></div>
+            <a href="check_list_error" id="error_btn"> 异常账单 </a>
             <div class="crossLine"></div>
             <a href="check_list_history" id="history_btn"> 历史 </a>
           </div>
@@ -50,10 +53,10 @@
                     </div>
                     <div class="crossLine"></div>
                     <div class="content_row blue_context">
-                      单价: 100.00
+                      总价: 100.00
                     </div>
                     <div class="content_row blue_context">
-                      数量: 2123123
+                      状态: 二次审核
                     </div>
                   </div>
                 </div>
