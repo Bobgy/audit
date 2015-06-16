e4ccecef2097339700da7be94f0a3a593b449479
chaiyujin <chaiyujin@gmail.com>
2015-06-17 00:12:56 +0800

    Update list.blade.php

--- resources/views/audit/list.blade.php
+++ resources/views/audit/list.blade.php
@@ -47,18 +47,72 @@
 
       <!-- Page -->
       <ul id="pagination-flickr">
-        <li class="previous-off">«Previous</li>
-        <li class="active">1</li>
-        <li><a href="?page=2">2</a></li>
-        <li><a href="?page=3">3</a></li>
-        <li><a href="?page=4">4</a></li>
-        <li><a href="?page=5">5</a></li>
-        <li><a href="?page=6">6</a></li>
-        <li><a href="?page=7">7</a></li>
-        <li class="next"><a href="?page=2">Next »</a></li>
+        <li id="pre" class="previous-off">«Previous</li>
+        <li id="p1" class="active">1</li>
+        <li id="p2"><a href="?page=2">2</a></li>
+        <li id="p3"><a href="?page=3">3</a></li>
+        <li id="p4"><a href="?page=4">4</a></li>
+        <li id="p5"><a href="?page=5">5</a></li>
+        <li id="p6"><a href="?page=6">6</a></li>
+        <li id="p7"><a href="?page=7">7</a></li>
+        <li id="next" class="next"><a href="?page=2">Next »</a></li>
       </ul>
       <!-- Page end -->
+      <script src="{{ asset('/lib/Flat-UI-master/dist/js/vendor/jquery.min.js') }}"></script>
+      <script>
+        var P = {{ $page }};
+        var PT = 10; 
+        var base = Div(P, 7) * 7;
+        for (var i = 1; i < 8; i++) {
+          var the_id = "#p" + i;
+          if (base + i <= PT) {
+            $(the_id).removeClass("active");
+            $(the_id).html("<a href='?page=" + (base + i) + "'>" + (base + i) + "</a>");
+          } else {
+            $(the_id).removeClass("active");
+            $(the_id).html("");
+          }
+        }
+        var now_id = "#p" + (P + 1 - base);
+        $(now_id).addClass("active");
+        $(now_id).html(P + 1);
+        
+        if (P > 0) {
+          $("#pre").removeClass("previous-off");
+          $("#pre").addClass("previous");
+          $("#pre").html("<a href='?page=" + P + "'>«Previous</a>");
+        } else {
+          $("#pre").removeClass("previous");
+          $("#pre").addClass("previous-off");
+          $("#pre").html("«Previous");
+        }
+        if (P < PT - 1) {
+          $("#next").removeClass("next-off");
+          $("#next").addClass("next");
+          $("#next").html("<a href='?page=" + (P + 2) + "'>Next »</a>");
+        } else {
+          $("#next").removeClass("next");
+          $("#next").addClass("next-off");
+          $("#next").html("Next »");
+        }
+        function Div(exp1, exp2) {
+            var n1 = Math.round(exp1); //四舍五入   
+            var n2 = Math.round(exp2); //四舍五入  
+
+            var rslt = n1 / n2; //除  
+
+            if (rslt >= 0) {
+                rslt = Math.floor(rslt); //返回小于等于原rslt的最大整数。   
+            }
+            else {
+                rslt = Math.ceil(rslt); //返回大于等于原rslt的最小整数。   
+            }
+
+            return rslt;
+        }
+      </script>
     </div>
   </div>
 
 </div>
+
