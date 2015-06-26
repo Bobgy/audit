40b1cb8cbf2c944656bf85936f3a1b5583b0800b
Bobgy <gongyuan94@gmail.com>
2015-06-26 14:14:04 +0800

    Removed unused information

--- resources/views/audit/main.blade.php
+++ resources/views/audit/main.blade.php
@@ -30,41 +30,14 @@
                   <div id="user_real_name" class="content_row">
 
                   </div>
+                  <div class="crossLine"></div>
                   <div id="user_id" class="content_row">
 
                   </div>
-                  <div class="crossLine"></div>
-                  <div id="user_kpi" class="content_row" style="color: #FF0000">
-                    绩效:
-                  </div>
+                  
                 </div>
               </div>
 
-              <div class="column_6">
-                <div class="content">
-                  <div id="processed" class="content_row">
-                    处理订单数：1000
-                  </div>
-                  <div class="crossLine"></div>
-                  <div id="unprocessed" class="content_row">
-                    待处理订单数：200
-                  </div>
-                  <div class="crossLine"></div>
-                  <div id="need_double" class="content_row">
-                    需二次审核：200
-                  </div>
-                </div>
-                <div class="content">
-                  <div id="processed_month" class="content_row">
-                    本月处理订单数：1000
-                  </div>
-                  <div class="crossLine"></div>
-                  <div id="processed_day" class="content_row">
-                    今日处理订单数：100
-                  </div>
-                  <div class="crossLine"></div>
-                </div>
-              </div>
             </div>
           </div>
           <!-- 个人信息结束 -->
@@ -110,13 +83,6 @@
 <script>
   $("#user_real_name").html("{{$real_name}}");
   $("#user_id").html("{{$user_id}}");
-  $("#user_kpi").html("绩效: 5.0");
-
-  $("#processed").html("处理订单: 1200");
-  $("#unprocessed").html("未处理订单: 1200");
-  $("#need_double").html("需二次审核: 1200");
-  $("#processed_month").html("本月处理订单: 1200");
-  $("#processed_day").html("本日处理订单: 1200");
 
 </script>
 
